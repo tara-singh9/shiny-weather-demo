4d53437f6204ecb2f7aaa68dfbe11c11ed697e8f
Tara Singh <tara.singh@duke.edu>
2024-04-23 14:04:10 -0400

    app creation!!!

--- app.R
+++ app.R
@@ -14,16 +14,58 @@ shinyApp(
     titlePanel("Weather Forecasts"),
     sidebarLayout(
       sidebarPanel(
-        # UI input code goes here
+        selectInput(
+          "city", "Select a city",
+          choices = c("Chicago", "Durham", "Sedona", "New York", "Los Angeles")
+        ),
+        selectInput(
+          "var", "Select a variable",
+          choices = c()
+        )
       ),
       mainPanel(
-        # UI output code goes here
+        tabsetPanel(
+          tabPanel("Plot", plotOutput("plot")),
+          tabPanel("Data", dataTableOutput("data"))
+        )
       )
     )
   ),
   server = function(input, output, session) {
 
-    # server code goes here
+    # weather for selected city
+    weather_city <- reactive({
+      weather |>
+        filter(city %in% input$city)
+    })
 
+    # weather variables that are numeric and not constant for selected city
+    weather_vars <- reactive({
+      weather_city() |>
+        select(where(is.numeric)) |>
+        select(where(function(x) var(x) != 0)) |>
+        names()
+    })
+
+    # update the select input when weather_vars changes
+    observe({
+      updateSelectInput(inputId = "var", choices = weather_vars())
+    })
+
+    # make plot
+    output$plot <- renderPlot({
+      req(input$var)
+      weather_city() |>
+        ggplot(aes_string(x = "time", y = input$var)) +
+        labs(title = paste(input$city, "-", input$var)) +
+        geom_line() +
+        theme_bw()
+    })
+
+    # make table
+    output$data <- renderDataTable({
+      weather_city() |>
+        select(city, time, input$var)
+    })
   }
 )
